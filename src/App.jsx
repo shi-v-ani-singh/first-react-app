bf94a73985bba01f2a00c832b5860bd64de66583
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2026-01-08 13:59:02 +0530

    my first react app created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, I am shivani.</h1>
+      <p>This is my first react app.</p>
+      <button>Click here...</button>
+
+    </div>
+  )
+}
+
+export default App
