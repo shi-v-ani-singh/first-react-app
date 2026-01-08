bf41324e1f66775547e7182059e87a462d8108bd
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2026-01-08 14:02:49 +0530

    created a input box

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       <h1>Hello, I am shivani.</h1>
       <p>This is my first react app.</p>
+      <input type="text" placeholder='write your name here' /><br />
       <button>Click here...</button>
 
     </div>
